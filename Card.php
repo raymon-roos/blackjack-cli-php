f1bdc9127449f2e95d0ab4cb72bfb4a68362ea53
Raymon Roos <raymon.roos@hotmail.com>
2023-10-02 20:11:39 +0200

    fix: put the actual files where blast can find them

---
+++ Card.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+class Card
+{
+	private Suit $suit;
+	private Value $value;
+
+	public function __construct(
+		string|Suit $suit,
+		int|string|Value $value
+	) {
+		$this->suit = match (gettype($suit)) {
+			'string' => Suit::fromString($suit),
+			'object' => $suit
+		};
+
+		$this->value = match (gettype($value)) {
+			'string', 'integer' => Value::from("$value"),
+			'object' => $value
+		};
+	}
+
+	public function show(): string
+	{
+		return "{$this->suit->value} {$this->value->value}";
+	}
+}
