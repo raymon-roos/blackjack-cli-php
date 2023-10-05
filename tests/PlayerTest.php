580fc81b0da18b8d4cecec6f6dc2b2eaad343055
Raymon Roos <raymon.roos@hotmail.com>
2023-10-05 09:10:47 +0200

    test: fix "can receive and show cards", add "can calculate hand score"

--- tests/PlayerTest.php
+++ tests/PlayerTest.php
@@ -13,20 +13,38 @@ final class PlayerTest extends MockeryTestCase
 	public function testCanReceiveAndShowCards(): void
 	{
 		$mockCard = Mockery::mock('App\Card');
-		$mockCard->shouldReceive('show')
-			->andReturn('♥ Q');
-
-		$mockDeck = Mockery::mock('App\Deck');
-		$mockDeck->shouldReceive('drawCard')
-			->andReturn($mockCard);
+		$mockCard->shouldReceive([
+			'show' => '♥ Q',
+			'getScore' => 10,
+		]);
 
 		$player = new Player('test');
 
-		$player->addCard($mockDeck->drawCard());
+		$player->addCard($mockCard);
 
 		$this->assertEquals(
 			'test has ♥ Q',
 			$player->showHand(),
+		);
+	}
+
+	public function testCanCalculateHandScore(): void
+	{
+		$player = new Player('test');
+
+		$mockCard = Mockery::mock('App\Card');
+		$mockCard->shouldReceive('getScore')
+			->andReturn(11);
+		$player->addCard($mockCard);
+
+		$mockCard = Mockery::mock('App\Card');
+		$mockCard->shouldReceive('getScore')
+			->andReturn(5);
+		$player->addCard($mockCard);
+
+		$this->assertEquals(
+			16,
+			$player->getHandScore(),
 		);
 	}
 }
