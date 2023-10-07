dcbbaa2596693d34ff0db168af79c1e565acc0a2
Raymon Roos <raymon.roos@hotmail.com>
2023-10-07 20:11:09 +0200

    feat: enumerate all possible player end states in `EndState`, with tests
    A little boilerplate-y, I admit.
    
    The benefit of using enums I have found so far, are that you can type
    hint them where ever you want to use their value. You can store the
    constructor logic in the enum itself, removing all validation logic out
    of the classes where you want to use the values. Same with string
    formatting logic. You can request a formatted string from a given enum
    when desired.
    
    Also, the enum classes can be tested in isolation, validating that the
    constructor and formatting and any other logic is correct, making it
    safe to rely on in other places, through type hinting.

---
+++ src/EndState.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use InvalidArgumentException;
+
+enum EndState: string
+{
+    case busted = 'Busted!';
+    case five_card_charlie = 'Five-Card-Charie!';
+    case twenty_one = 'Twenty-one!';
+    case blackjack = 'Blackjack!';
+    case folded = 'folded';
+
+    public static function tryFromHandScore(int $score, int $count): ?self
+    {
+        return match (true) {
+            $score > 21                   => self::busted,
+            $score < 21 && $count === 5   => self::five_card_charlie,
+            $score === 21 && $count === 2 => self::blackjack,
+            $score === 21                 => self::twenty_one,
+            default                       => null
+        };
+    }
+
+    public static function fromString(string $state): ?self
+    {
+        return match (strtolower(trim($state))) {
+            'busted'            => self::busted,
+            'five-card-charlie' => self::five_card_charlie,
+            'twenty-ony'        => self::twenty_one,
+            'blackjack'         => self::blackjack,
+            'folded'            => self::folded,
+            default             => throw new InvalidArgumentException("$state is not a valid end state")
+        };
+    }
+}
---
+++ tests/EndStateTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use App\EndState;
+use InvalidArgumentException;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+final class EndStateTest extends MockeryTestCase
+{
+	public function testCanMakeEndStateFromScores(): void
+	{
+		$this->assertEquals(EndState::busted, EndState::tryFromHandScore(22, 3));
+		$this->assertEquals(EndState::five_card_charlie, EndState::tryFromHandScore(20, 5));
+		$this->assertEquals(EndState::twenty_one, EndState::tryFromHandScore(21, 3));
+		$this->assertEquals(EndState::blackjack, EndState::tryFromHandScore(21, 2));
+		$this->assertEquals(null, EndState::tryFromHandScore(20, 2));
+	}
+
+	public function testCanMakeEndStateFromString(): void
+	{
+		$this->assertEquals(EndState::busted, EndState::fromString('busted'));
+		$this->assertEquals(EndState::five_card_charlie, EndState::fromString('five-card-charlie'));
+		$this->assertEquals(EndState::twenty_one, EndState::fromString('twenty-ony'));
+		$this->assertEquals(EndState::blackjack, EndState::fromString('blackjack'));
+	}
+
+	public function testThrowsOnInvalidArgument(): void
+	{
+		$this->expectExceptionObject(
+			new InvalidArgumentException('test is not a valid end state')
+		);
+
+		EndState::fromString('test');
+	}
+}
