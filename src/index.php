39dd79900e6a42ac495ea97c87bb60f796a06f55
Raymon Roos <raymon.roos@hotmail.com>
2023-10-14 14:28:18 +0200

    feat: add `-s` / `--simulate` cli flags for playing a simulated game

--- src/index.php
+++ src/index.php
@@ -6,5 +6,23 @@ namespace App;
 
 require_once('vendor/autoload.php');
 
-($game = new Blackjack())
+if (isset($argv[1]) && in_array(trim($argv[1]), ['-s', '--simulate'])) {
+	// For manual testing perposes
+
+	// Automatically run through a full game, with seven players numbered one
+	// through seven, that will always draw as many cards as possible 
+
+	for ($i = 1; $i < 8; $i++) { 
+		$players[] = new Player("$i");
+	}
+
+	$fakePrompter = new Class extends UserPrompter {
+		public function promptForPlayerToDrawCard(): bool
+		{
+			return true;
+		}
+	};
+}
+
+($game = new Blackjack($players ?? null, userPrompter: $fakePrompter ?? new UserPrompter()))
 	->playGame();
