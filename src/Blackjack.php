5ca031567e7df05f2381cfcce97ced8ab8b3a8f9
Raymon Roos <raymon.roos@hotmail.com>
2023-10-14 14:27:08 +0200

    feat: rework `Blackjack` to use a `Dealer`, with tests
    Now player play against the dealer. The game ends when the dealer is out
    of the game, either because they win, lose, or stand at 17 points. After
    all players have played their turns to fruition, the final scores are
    printed comparing each players' score to that of the dealer. The game
    can end early if the dealer wins, loses, or stands after the opening
    hand is dealt.
    
    The tests for the `Blackjack` class are horrible, there is so much
    duplication between the large amounts of mocking that are required.

--- src/Blackjack.php
+++ src/Blackjack.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App;
 
-use InvalidArgumentException;
-
 class Blackjack
 {
 	/**
@@ -14,33 +12,37 @@ class Blackjack
 	private array $players;
 
 	/**
-	 * @param Deck $deck
-	 * @param Player[] $players
+	 * @param null|Player[] $players
 	 */
-	public function __construct(
-		private Deck $deck = new Deck(),
-		?array $players = null,
-		private UserPrompter $userPrompter = new UserPrompter(),
-	) {
-		$this->players = $players ?? $this->addPlayers();
+    public function __construct(
+        ?array $players = null,
+        private Dealer $dealer = new Dealer(),
+        private UserPrompter $userPrompter = new UserPrompter(),
+    ) {
+		$players = array_values(array_filter(
+            $players ??= $this->addPlayers(),
+            fn ($input) => $input instanceof Player
+        ));
 
-		if (empty($this->players)) {
-			throw new InvalidArgumentException('No players given');
-		}
-	}
+        if (empty($players)) {
+			throw new \InvalidArgumentException('No players given');
+        }
+
+		$this->players = [$this->dealer, ...$players];
+    }
 
 	private function addPlayers(): ?array
 	{
-		echo 'Welcome to blackjack!' . PHP_EOL 
+		echo 'Welcome to blackjack!' . PHP_EOL
 		. 'Up to 7 players are allowed to participate' . PHP_EOL;
 
         while (
-            count($players ??= []) < 7 
+            count($players ??= []) < 7
 			&& $playerName = $this->userPrompter->promptForPlayerName()
         ) {
 			try {
                 $players[] = new Player(trim($playerName));
-            } catch (\Throwable) {
+            } catch (\InvalidArgumentException) {
 				echo 'A name is required, please try again' . PHP_EOL;
             }
         }
@@ -50,69 +52,80 @@ class Blackjack
 
 	public function playGame(): void
 	{
-		$this->dealFirstHand();
+		echo 'Starting the game now' . PHP_EOL;
 
-		foreach ($this->players as $player) {
-			if (!$player->isFinished()) {
+        $this->dealFirstHand();
+
+		while (
+			!$this->dealer->isFinished()
+        ) {
+            foreach ($this->players as $player) {
+				if ($player->isFinished()) {
+					continue;
+				}
+
 				$this->processPlayerTurn($player);
-			}
-		}
 
-		$standingPlayers = array_filter(
-			$this->players,
-			fn (Player $player) => $player->showState() === 'stands'
-		);
-
-		// If more than one player stands, find
-		// the player with a score closest to 21
-		if (count($standingPlayers) >= 2) {
-			$this->processStandingPlayers($standingPlayers);
+				if ($this->dealer->getState() === EndState::busted) {
+					break;
+				}
+            }
         }
+
+		$this->showScores();
     }
 
 	private function dealFirstHand(): void
 	{
 		foreach ($this->players as $player) {
-			$player->addCard($this->deck->drawCard());
-			$player->addCard($this->deck->drawCard());
+			$this->dealer->deal($player);
+			$this->dealer->deal($player);
 			echo $player->showHand() . PHP_EOL;
-			echo PHP_EOL . $player->showState() . PHP_EOL;
 		}
 	}
 
 	private function processPlayerTurn(Player $player): void
 	{
 		echo PHP_EOL . "Your turn {$player->name}" . PHP_EOL;
+		echo 'Currently, ' . $player->showHand() . PHP_EOL;
+
+		match (true) {
+			$player instanceof Dealer,
+			$this->userPrompter->promptForPlayerToDrawCard() => $this->dealer->deal($player),
+			default => $player->stand()
+		};
+
 		echo $player->showHand() . PHP_EOL;
-
-		while (!$player->isFinished()) {
-			if (!$this->userPrompter->promptForPlayerToDrawCard()) {
-				$player->stand();
-				echo $player->showHand() . PHP_EOL;
-				break;
-			}
-
-			$player->addCard($this->deck->drawCard());
-			echo $player->showHand() . PHP_EOL;
-		}
-
-		echo PHP_EOL . $player->showState() . PHP_EOL;
 	}
 
-	private function processStandingPlayers(array $players): void
+	private function showScores(): void
 	{
-		foreach ($players as $player) {
-			$topPlayer ??= $player;
-			if ($player->getHandScore() > $topPlayer->getHandScore()) {
-				$topPlayer = $player;
+		echo PHP_EOL . 'Final scores:' . PHP_EOL;
+
+		$dealerScore = $this->dealer->getHandScore();
+		$dealerState = $this->dealer->getState();
+
+		if ($dealerState === EndState::busted) {
+			echo 'Dealer lost, all players win!' . PHP_EOL;
+			return;
+        }
+
+		array_shift($this->players);
+
+		foreach ($this->players as $player) {
+			if ($player->isFinished()) {
+                echo $player->showState() . PHP_EOL;
+                continue;
 			}
+
+			$playerScore = $player->getHandScore();
+
+			$pointsDifference = $dealerScore - $playerScore;
+			echo match (true) {
+				$pointsDifference > 0 => "$player->name lost by by $pointsDifference points",
+				$pointsDifference === 0 => "$player->name is at a standoff",
+				$pointsDifference < 0 => "$player->name won by by " . abs($pointsDifference) . ' points',
+			} . PHP_EOL;
 		}
-
-		echo <<<OUTPUT
-
-		Of all players that chose to stand, {$topPlayer->name} got closest to 21,
-		{$topPlayer->showHand()}
-		
-		OUTPUT;
 	}
 }
--- tests/BlackjackTest.php
+++ tests/BlackjackTest.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Tests;
 
 use App\Blackjack;
-use App\Card;
-use App\Rank;
-use App\Suit;
+use App\Dealer;
+use App\EndState;
+use App\Player;
 use InvalidArgumentException;
 use Mockery;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
@@ -30,17 +30,22 @@ final class BlackjackTest extends MockeryTestCase
 			'promptForPlayerToDrawCard' => false,
 		]);
 
-		$mockPlayer = Mockery::mock('App\Player');
-		$mockPlayer->shouldReceive([
-			'addCard' => null,
-			'stand' => null,
-			'isFinished' => false,
-			'showState'  => 'Katherine Johnson stands',
-			'showHand' => 'Katherine Johnson has ♥ 9001',
+		$player = new Player('Katherine Johnson');
+
+		$mockCard = Mockery::mock('App\Card');
+		$mockCard->shouldReceive([
+			'getScore' => 7,
+			'show' => '♥ 7',
 		]);
 
-		(new Blackjack(userPrompter: $mockPrompter, players: [$mockPlayer]))
-			->playGame();
+		$mockDeck = Mockery::mock('App\Deck');
+		$mockDeck->shouldReceive('drawCard')
+			->andReturn($mockCard);
+
+		$dealer = new Dealer(deck: $mockDeck);
+
+		$game = new Blackjack([$player], $dealer, $mockPrompter);
+		$game->playGame();
 
 		$this->expectOutputRegex('/.*Welcome!.*/');
 		$this->expectOutputRegex('/.*Katherine Johnson has ♥ 9001.*/');
@@ -48,101 +53,143 @@ final class BlackjackTest extends MockeryTestCase
 		$this->expectOutputRegex('/.*Katherine Johnson stands.*/');
 	}
 
+	public function testDealerDrawsUntilAtLeast17Points(): void
+   	{
+		$mockCard = Mockery::mock('App\Card');
+		$mockCard->shouldReceive('getScore')
+			->andReturn(10, 7);
+		$mockCard->shouldReceive('show')
+			->andReturn('♥ 10', '♥ 7');
+
+		$mockDeck = Mockery::mock('App\Deck');
+		$mockDeck->shouldReceive('drawCard')
+			->andReturn($mockCard);
+
+		$player = new Player('Bletchley Park');
+
+		$mockPrompter = Mockery::mock('App\UserPrompter');
+		$mockPrompter->shouldReceive(['promptForPlayerToDrawCard' => true]);
+
+		$dealer = new Dealer(deck: $mockDeck);
+
+		$game = new Blackjack([$player], $dealer, $mockPrompter);
+		$game->playGame();
+
+		$this->assertEquals(EndState::stands, $dealer->getState());
+		$this->expectOutputRegex('/.*Welcome to blackjack!.*/');
+		$this->expectOutputRegex('/.*Starting the game now.*/');
+		$this->expectOutputRegex('/.*Your turn dealer.*/');
+		$this->expectOutputRegex('/.*dealer has ♥ 10 ♥ 7 (17).*/');
+		$this->expectOutputRegex('/.*dealer stands.*/');
+	}
+
 	public function testPlayersCanGetBusted(): void
 	{
+		$mockCard = Mockery::mock('App\Card');
+		$mockCard->shouldReceive('getScore')
+			->andReturn(5, 11);
+		$mockCard->shouldReceive('show')
+			->andReturn('♥ 5', '♥ A');
+
+		$mockDeck = Mockery::mock('App\Deck');
+		$mockDeck->shouldReceive('drawCard')
+			->andReturn($mockCard);
+
 		$mockDeck = Mockery::mock('App\Deck');
 		$mockDeck->shouldReceive([
-			'drawCard' => new Card(Suit::hearts, Rank::queen)
+			'drawCard' => $mockCard,
 		]);
 
 		$mockPrompter = Mockery::mock('App\UserPrompter');
-		$mockPrompter->shouldReceive([
-			'promptForPlayerNames' => ['Mary Kenneth Keller'],
-			'promptForPlayerToDrawCard' => true,
-		]);
+		$mockPrompter->shouldReceive('promptForPlayerName')
+			->andReturn('Mary Kenneth Keller', '');
+		$mockPrompter->shouldReceive('promptForPlayerToDrawCard')
+			->andReturn(true);
 
-		(new Blackjack($mockDeck, userPrompter: $mockPrompter))
-			->playGame();
+		$dealer = new Dealer(deck: $mockDeck);
 
-		$this->expectOutputRegex('/.*Welcome!.*/');
-		$this->expectOutputRegex('/.*Your turn Mary Kenneth Keller.*/');
-		$this->expectOutputRegex('/.*Mary Kenneth Keller has ♥ 10 ♥ 10 \(20\).*/');
-		$this->expectOutputRegex('/.*Mary Kenneth Keller has ♥ 10 ♥ 10 ♥ 10 \(30\).*/');
-		$this->expectOutputRegex('/.*Mary Kenneth Keller is Busted!.*/');
+		$game = new Blackjack(dealer: $dealer, userPrompter: $mockPrompter);
+		$game->playGame();
+
+		$this->expectOutputRegex('/\s*Welcome to blackjack!\s*/');
+		$this->expectOutputRegex('/\s*Starting the game now\s*/');
+		$this->expectOutputRegex('/\s*Your turn dealer\s*/');
+		$this->expectOutputRegex('/\s*dealer has ♥ 5 ♥ A \(11\)\s*/');
+		$this->expectOutputRegex('/\s*Mary Kenneth Keller has ♥ A ♥ A \(22\)\s*/');
+		$this->expectOutputRegex('/\s*Mary Kenneth Keller is Busted!\s*/');
 	}
 
 	public function testPlayersCanGetBlackjack(): void
 	{
+		$mockCard = Mockery::mock('App\Card');
+		$mockCard->shouldReceive('getScore')
+			->andReturn(5, 11, 10, 11);
+		$mockCard->shouldReceive('show')
+			->andReturn('♥ 5', '♥ A', '♥ 10', '♥ A');
+
 		$mockDeck = Mockery::mock('App\Deck');
 		$mockDeck->shouldReceive('drawCard')
-			->andReturn(
-				new Card(Suit::hearts, Rank::ace),
-				new Card(Suit::hearts, Rank::queen),
-			);
+			->andReturn($mockCard);
 
-		$mockPrompter = Mockery::mock('App\UserPrompter');
-		$mockPrompter->shouldReceive([
-			'promptForPlayerNames' => ['Joy Buolamwini'],
+		$mockDeck = Mockery::mock('App\Deck');
+		$mockDeck->shouldReceive([
+			'drawCard' => $mockCard,
 		]);
 
-		(new Blackjack($mockDeck, userPrompter: $mockPrompter))
-			->playGame();
+		$mockPrompter = Mockery::mock('App\UserPrompter');
+		$mockPrompter->shouldReceive('promptForPlayerName')
+			->andReturn('Joy Buolamwini', '');
+		$mockPrompter->shouldReceive('promptForPlayerToDrawCard')
+			->andReturn(true);
 
-		$this->expectOutputRegex('/.*Welcome!.*/');
+		$dealer = new Dealer(deck: $mockDeck);
+
+		$game = new Blackjack(dealer: $dealer, userPrompter: $mockPrompter);
+		$game->playGame();
+
+		$this->expectOutputRegex('/\s*Welcome to blackjack!\s*/');
+		$this->expectOutputRegex('/\s*Starting the game now\s*/');
+		$this->expectOutputRegex('/\s*Your turn dealer\s*/');
+		$this->expectOutputRegex('/\s*dealer has ♥ 5 ♥ A \(11\)\s*/');
 		$this->expectOutputRegex('/.*Your turn Joy Buolamwini.*/');
-		$this->expectOutputRegex('/.*Joy Buolamwini has ♥ A ♥ Q \(21\).*/');
+		$this->expectOutputRegex('/.*Joy Buolamwini has ♥ 10 ♥ A \(21\).*/');
 		$this->expectOutputRegex('/.*Joy Buolamwini has Blackjack!.*/');
 	}
 
-	public function testPlayersCanGetTwentyOne(): void
+	public function testGameEndsWhenDealerIsBusted(): void
 	{
+		$mockCard = Mockery::mock('App\Card');
+		$mockCard->shouldReceive('getScore')
+			->andReturn(11, 11);
+		$mockCard->shouldReceive('show')
+			->andReturn('♥ A', '♥ A');
+
 		$mockDeck = Mockery::mock('App\Deck');
 		$mockDeck->shouldReceive('drawCard')
-			->andReturn(
-				new Card(Suit::hearts, Rank::ace),
-				new Card(Suit::hearts, Rank::four),
-				new Card(Suit::hearts, Rank::six),
-			);
+			->andReturn($mockCard);
 
-		$mockPrompter = Mockery::mock('App\UserPrompter');
-		$mockPrompter->shouldReceive([
-			'promptForPlayerNames' => ['Joy Buolamwini'],
-			'promptForPlayerToDrawCard' => true,
-		]);
-
-		(new Blackjack($mockDeck, userPrompter: $mockPrompter))
-			->playGame();
-
-		$this->expectOutputRegex('/.*Welcome!.*/');
-		$this->expectOutputRegex('/.*Your turn Joy Buolamwini.*/');
-		$this->expectOutputRegex('/.*Joy Buolamwini has ♥ A ♥ 4 ♥ 6 \(21\).*/');
-		$this->expectOutputRegex('/.*Joy Buolamwini has Twenty-One!.*/');
-	}
-
-	public function testPlayersCanGetFiveCardCharlie(): void
-	{
 		$mockDeck = Mockery::mock('App\Deck');
-		$mockDeck->shouldReceive('drawCard')
-			->andReturn(
-				new Card(Suit::hearts, Rank::two),
-				new Card(Suit::hearts, Rank::three),
-				new Card(Suit::hearts, Rank::four),
-				new Card(Suit::hearts, Rank::five),
-				new Card(Suit::hearts, Rank::six),
-			);
-
-		$mockPrompter = Mockery::mock('App\UserPrompter');
-		$mockPrompter->shouldReceive([
-			'promptForPlayerNames' => ['Joy Buolamwini'],
-			'promptForPlayerToDrawCard' => true,
+		$mockDeck->shouldReceive([
+			'drawCard' => $mockCard,
 		]);
 
-		(new Blackjack($mockDeck, userPrompter: $mockPrompter))
-			->playGame();
+		$mockPrompter = Mockery::mock('App\UserPrompter');
+		$mockPrompter->shouldReceive('promptForPlayerName')
+			->andReturn('Joy Buolamwini', '');
+		$mockPrompter->shouldReceive('promptForPlayerToDrawCard')
+			->andReturn(true);
 
-		$this->expectOutputRegex('/.*Welcome!.*/');
-		$this->expectOutputRegex('/.*Your turn Joy Buolamwini.*/');
-		$this->expectOutputRegex('/.*Joy Buolamwini has ♥ 2 ♥ 3 ♥ 4 ♥ 5 ♥ 6 \(21\).*/');
-		$this->expectOutputRegex('/.*Joy Buolamwini has Five-Card-Charlie!.*/');
+		$dealer = new Dealer(deck: $mockDeck);
+
+		$game = new Blackjack(dealer: $dealer, userPrompter: $mockPrompter);
+		$game->playGame();
+
+		$this->expectOutputRegex('/\s*Welcome to blackjack!\s*/');
+		$this->expectOutputRegex('/\s*Starting the game now\s*/');
+		$this->expectOutputRegex('/\s*Your turn dealer\s*/');
+		$this->expectOutputRegex('/\s*dealer has ♥ A ♥ A \(22\) dealer is Busted!\s*/');
+		$this->expectOutputRegex('/.*Joy Buolamwini has ♥ A ♥ A \(22\) Joy Buolamwini is Busted!.*/');
+		$this->expectOutputRegex('/\s*dealer has ♥ A ♥ A \(22\) dealer is Busted!\s*/');
+		$this->expectOutputRegex('/\s*Final scores:\s+Dealer lost, all players win!\s*/');
 	}
 }
